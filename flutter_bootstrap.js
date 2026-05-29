cff43f2938198cdfc138c2e271f6e047fe9de4f4
Rodrigo Rocha <rodrigorgs@gmail.com>
2026-05-29 16:07:40 -0300

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4026842447"
+    serviceWorkerVersion: "3700472335"
   }
 });
